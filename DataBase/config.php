09ceb1b385622bf7e1e6b37b1c456f01aa2c358d
Matheus_Barreto <matheusbarreto.dev@gmail.com>
2023-06-27 23:51:05 -0300

    PDO
    Alteração para uso de PDO

--- DataBase/config.php
+++ DataBase/config.php
@@ -6,5 +6,17 @@
     define('BASE','vetclinic');
 
     /*--iniciando conexão com o banco--*/
-    $conn = new mysqli(HOST,USER,PASS,BASE);
+    $dsn = "mysql:host=" . HOST . ";dbname=" . BASE;
+    $options = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    );
+
+    try {
+        $conn = new PDO($dsn, USER, PASS, $options);
+    } catch(PDOException $e) {
+        echo "Falha na conexão: " . $e->getMessage();
+    }
+
+    
 ?>
