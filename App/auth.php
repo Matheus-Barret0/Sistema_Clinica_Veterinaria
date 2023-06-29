1357f242cafacef541c14c2644165408793f2373
Matheus_Barreto <matheusbarreto.dev@gmail.com>
2023-06-28 23:04:06 -0300

    Correção no arquivo auth.php para uso do PDO
    correções no arquivo auth.php para garantir o uso adequado do PDO (PHP Data Objects).

--- App/auth.php
+++ App/auth.php
@@ -1,31 +1,36 @@
 <?php
-require_once 'C:\xampp\htdocs\Sistema_de_Cadastro_Clinica_Veterinaria\DataBase\config.php';
+require_once 'C:\xampp\htdocs\Sistema_Clinica_Veterinaria\DataBase\config.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["user"];
     $password = $_POST["password"];
 
     $sql = "SELECT id, user_name, password FROM user
-        WHERE user_name = '$username' AND password = '$password'";
+        WHERE user_name = :username AND password = :password";
 
-    $result = mysqli_query($conn, $sql);
+    try {
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':password', $password);
+        $stmt->execute();
 
-    if (mysqli_num_rows($result) == 1) {
-        // Autenticação bem-sucedida
-        $row = mysqli_fetch_assoc($result);
-        $id = $row['id'];
-        $user_name = $row['user_name'];
-        session_start();
-        $_SESSION['id'] = $id;
-        $_SESSION['user_name'] = $user_name;
+        if ($stmt->rowCount() == 1) {
+            // Autenticação bem-sucedida
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            $id = $row['id'];
+            $user_name = $row['user_name'];
+            session_start();
+            $_SESSION['id'] = $id;
+            $_SESSION['user_name'] = $user_name;
 
-        header("Location: /Sistema_de_Cadastro_Clinica_Veterinaria/Views");
-        exit();
-    } else {
-        // Usuário ou senha incorretos
-        $error_message = "Usuário ou senha incorreto.";
+            header("Location: /Sistema_Clinica_Veterinaria/Views");
+            exit();
+        } else {
+            // Usuário ou senha incorretos
+            $error_message = "Usuário ou senha incorreto.";
+        }
+    } catch(PDOException $e) {
+        echo "Erro na consulta: " . $e->getMessage();
     }
 }
-
-mysqli_close($conn);
 ?>
